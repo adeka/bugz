34ce9e344efaed2925653b1ca07a7ca53ebd30d0
Nikita Filatov <nfilatov@nfilatov-mbp.local>
2016-12-27 15:30:26 -0500

    scaffold

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    entry: __dirname + "/js/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    resolve: {
+        alias: {
+        },
+        extensions: ['', '.js', '.json']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.json/,
+                loader: ["json"]
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015'],
+                    plugins: [
+                        "transform-decorators-legacy",
+                        "transform-object-rest-spread",
+                        "syntax-class-properties",
+                        "transform-class-properties"
+                    ]
+                }
+            },
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
+            {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
+            {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"}
+        ]
+    },
+};
